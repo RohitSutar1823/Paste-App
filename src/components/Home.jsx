2f3b92c7a89562cedc10e90394b9af15783fa847
Rohit Sutar <rohit1823sutar@gmail.com>
2026-09-09 19:55:41 +0530

    feat: enhance Home component with input field for text entry and update Navbar layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Home = () => {
+  const [title, setTitle] = useState('')
   return (
     <div>
-      Home
+      <input
+      className='p-2 rounded-2xl mt-2'
+      type="text"
+      placeholder='enter the text'
+      value={title}
+      onChange={(e)=>setTitle(e.target.value)}
+      />
     </div>
   )
 }
